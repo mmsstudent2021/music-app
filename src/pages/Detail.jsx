3a2bed28eacf12a0f9f06289d1af2f8ed035aa29
mmsstudent2021 <mmsstudent2021@gmial.com>
2023-01-22 08:31:43 +0630

    cm

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -33,6 +33,9 @@ const Detail = () => {
       <a target={"_blank"} href={url} className="text-white bg-blue-700 px-10 py-3 rounded shadow-sm">
         download - {id}
       </a>
+      <button onClick={() => console.log(url)} className="text-white bg-blue-700 px-10 py-3 rounded shadow-sm">
+        download - {id}
+      </button>
       <h1>We love Min Thiha</h1>
     </div>
   );
